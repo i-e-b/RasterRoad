d6160776842dced0d3c080429db75113cf3ae118
i-e-b <ieb@me.com>
2017-05-29 21:40:41 +0100

    2 texture, flowing, nicer hills.

--- main.lua
+++ main.lua
@@ -2,7 +2,7 @@
 
 local screenWidth, screenHeight
 local shader, mesh
-local texture1, offsetImg, offsetData
+local texture1, texture2, offsetImg, offsetData
 local time = 0
 
 
@@ -11,6 +11,7 @@ function love.load()
   screenWidth, screenHeight = love.graphics.getDimensions( )
 
   texture1 = love.graphics.newImage("assets/road.png")
+  texture2 = love.graphics.newImage("assets/road2.png")
 
   offsetData = love.image.newImageData(1,screenHeight)
   offsetImg = love.graphics.newImage( offsetData ) -- store scanline offsets
@@ -24,23 +25,47 @@ function love.load()
   }, "triangles" )
 
   shader = love.graphics.newShader(
-[[
+  [[
   extern Image offsets; // shift -1..1 encoded in 16 bits across the
                         // red and green channels.
+                        // z position encoded in 16 bits across blue and alpha
+
+  extern Image alternate; // the 'other' scanline image. This is used to fake road
+                          // lines and distance. The main and alt images should be
+                          // less different as distance increases, to limit aliasing.
+
+  extern float movement; // increase proportional to speed.
+
+  extern float fog; // higher is more foggy
 
   vec4 effect(vec4 color, Image texture, vec2 tc, vec2 sc) {
-    float z = (1 - tc.y) + 1;
+    float haze = pow((1 - tc.y) + 1, fog);
+
+    // X offset, for left right curvature of road
     float fx = (Texel(offsets, vec2(0, tc.y)).r) - 0.5;
     fx += (Texel(offsets, vec2(0, tc.y)).g - 0.5) / 254;
-    float fy = (Texel(offsets, vec2(0, tc.y)).b);
-    return Texel(texture, vec2(tc.x + fx, fy)); // divide by z to get night effect
+
+    // Y offset, to simulate Z position of road
+    float fy = (Texel(offsets, vec2(0, tc.y)).b) * 2;
+    fy += (Texel(offsets, vec2(0, tc.y)).a) / 127;
+
+    // switch between two textures to simulate scanline palette swap effect
+    // switches happen more often at higher 'z-distance'
+    float checker = floor(mod((2 / fy) + movement, 1) * 2); // change constant to alter checker scale
+    checker = clamp(checker,0,1);
+
+    vec4 sample1 = Texel(texture, vec2(tc.x + fx, fy));
+    vec4 sample2 = Texel(alternate, vec2(tc.x + fx, fy));
+
+    return mix(sample1, sample2, checker) / haze; // divide by z to get haze effect
   }
-]] )
+  ]] )
+  shader:send("alternate", texture2)
 end
 
 function love.update(dt)
   if (dt > 0.1) then return end
-  time = time + (dt * 24)
+  time = time + (dt )
   updateScanlines ()
 end
 
@@ -48,25 +73,38 @@ function sat01(v)
   return math.min(1,math.max(0, v))
 end
 
+-- split a number between two bytes
+function splitBytes(n)
+  local upper = 32258 * n
+  local lower = upper % 254
+  return upper / 254, lower
+end
+
 function updateScanlines ()
+  local y = love.mouse.getY() / screenHeight
   -- left/right shift is encoded into red and green channels for precision
-  -- TODO: encode virtual 'z' into blue, and shift tc.y based on that.
+  -- virtual 'z' is encoded into blue to shift texture for hills.
+  local z = 0
+  local gamma = 1.5 * y + 0.5 -- hills < 1 > valleys
   for i=0, (screenHeight-1) do
-    local wiggle = (math.sin((i+time) / 40) / 20) + 1
-    local upper = 32258 * wiggle
-    local lower = upper % 254
-    local z = sat01((i) / screenHeight)
-    offsetData:setPixel(0, i, upper / 254, lower, z * 255, 255)
+    local xupper, xlower = splitBytes(i / screenHeight)
+
+    z = math.pow(i / screenHeight, gamma)
+    local zupper, zlower = splitBytes(z)
+    offsetData:setPixel(0, i, xupper, xlower, zupper, zlower)
   end
   offsetImg = love.graphics.newImage( offsetData )
 end
 
 function love.draw()
-    shader:send("offsets", offsetImg)
+  love.graphics.setBackgroundColor( 112, 159, 237 )
+  shader:send("offsets", offsetImg)
+  shader:send("movement", time * 3) -- 10 x time is abut the limit.
+  shader:send("fog", 0.3)
 
-    love.graphics.setShader(shader)
-    mesh:setTexture( texture1 )
-    --mesh:setTexture( offsetImg )
-    love.graphics.draw(mesh, 0, 0)
-    love.graphics.setShader()
+  love.graphics.setShader(shader)
+  mesh:setTexture( texture1 )
+  --mesh:setTexture( offsetImg )
+  love.graphics.draw(mesh, 0, 0)
+  love.graphics.setShader()
 end
